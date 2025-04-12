f1cba2a54ae59b709f287d863a19b853637224d0
Agustin <tenagliaa11@gmail.com>
2025-04-12 09:19:49 -0300

    adding tge second exercise

--- exercise1-2.c
+++ exercise1-2.c
@@ -1,5 +1,18 @@
+/* 
+Se ingresan la cantidad de horas trabajadas y el valor por hora de un empleado. Determinar el sueldo. 
+*/
+
 #include <stdio.h>
 
 int main() {
+  int horasTrabajadas, valorDeCadaHora;
+  float sueldo;
+  
+  printf("ingrese la cantidad de horas tranajadas: ");
+  scanf("%d", &horasTrabajadas);
+  printf("ahora el valor de cada hora");
+  scanf("%d", &valorDeCadaHora);
+  sueldo = (horasTrabajadas * valorDeCadaHora) * 1.0;
+  printf("tu sueldo es: %f ", sueldo);
   return 0;
 }
